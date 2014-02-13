436cf6db7315f0c5641d160c812c3160dc487412
Christopher Van <cvan@mozilla.com>
2014-02-12 18:01:38 -0800

    fix app name in server initialisation

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var restifySwagger = require('node-restify-swagger');
 var restifyValidation = require('node-restify-validation');
 
 var server = restify.createServer({
-    name: 'loma',
+    name: 'firesnaggle',
     version: '0.0.1'
 });
 
